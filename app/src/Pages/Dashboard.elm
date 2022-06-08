6abe0e58801fe0d30fdb814cbe011247abe1fc65
Naora <jorisgundermann@gmail.com>
2022-06-08 23:09:23 +0200

    format elm files

--- app/src/Pages/Dashboard.elm
+++ app/src/Pages/Dashboard.elm
@@ -1,27 +1,31 @@
 module Pages.Dashboard exposing (..)
 
+import Debug exposing (toString)
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 import Http exposing (..)
-import Session exposing (..)
 import Json.Decode as De
 import Json.Encode as En
 import Router exposing (Route(..))
-import Html.Events exposing (onClick)
-import Debug exposing (toString)
+import Session exposing (..)
 
 
 type Msg
     = GotPages (Result Http.Error (Response Overview))
     | CreateButtonClicked
-    | CreatePage (Result Http.Error (Response String)) 
+    | CreatePage (Result Http.Error (Response String))
 
-type alias Response data = 
-    { data: Maybe data
-    , links: List String
+
+type alias Response data =
+    { data : Maybe data
+    , links : List String
     }
 
-type alias Overview =  List String
+
+type alias Overview =
+    List String
+
 
 type PageState
     = Loading
@@ -52,106 +56,132 @@ update msg model =
 
                 Err err ->
                     ( { model | state = Failure err }, Cmd.none )
-        
+
         CreateButtonClicked ->
-                    ({model | state = Loading}, createPage)
+            ( { model | state = Loading }, createPage )
 
         CreatePage result ->
             case result of
                 Ok _ ->
-                    (model, loadPages)
+                    ( model, loadPages )
 
                 Err err ->
-                    ({model | state = Failure err}, Cmd.none)
+                    ( { model | state = Failure err }, Cmd.none )
+
+
 
 -- Views
+
 
 view : Model -> Html Msg
 view model =
     case model.state of
         Loading ->
             loadingView
+                |> withLayout
 
         Success overview ->
             successView overview
+                |> withLayout
 
         Failure error ->
             failureView error
+                |> withLayout
 
 
-loadingView : Html msg
+withLayout : List (Html msg) -> Html msg
+withLayout children =
+    div [] children
+
+
+loadingView : List (Html msg)
 loadingView =
-    div []
-        [ text "Loading"
-        ]
+    [ div [] [ text "Loading" ] ]
 
 
-successView : Response Overview -> Html Msg
+successView : Response Overview -> List (Html Msg)
 successView overview =
     case overview.data of
         Just data ->
-            div [] (addPageForm :: (List.map pageView data))
-        
+            [ div [] (addPageForm :: List.map pageView data) ]
+
         Nothing ->
-            div [] [text "No page yet"]
+            [ div [] [ text "No page yet" ] ]
 
 
 addPageForm : Html Msg
-addPageForm = button [onClick CreateButtonClicked] [text "Create Page"] 
+addPageForm =
+    button [ onClick CreateButtonClicked ] [ text "Create Page" ]
+
 
 pageView : String -> Html msg
 pageView page =
-    div [] [text page]
+    div [] [ text page ]
 
 
-failureView : Http.Error -> Html msg
+failureView : Http.Error -> List (Html msg)
 failureView error =
-    case error of
-        BadUrl url ->
-            div[] [text ("Bad Url " ++ url)]
+    let
+        display =
+            case error of
+                BadUrl url ->
+                    div [] [ text ("Bad Url " ++ url) ]
 
-        Timeout ->
-            div[] [text ("We waited for ages")]
+                Timeout ->
+                    div [] [ text "We waited for ages" ]
 
-        NetworkError ->
-            div[] [text ("Oups no network")]
+                NetworkError ->
+                    div [] [ text "Oups no network" ]
 
-        BadStatus status ->
-            div[] [text ("Bad status " ++ toString status)]
+                BadStatus status ->
+                    div [] [ text ("Bad status " ++ toString status) ]
 
-        BadBody body ->
-            div[] [text ("Bad Body : " ++ body)]
+                BadBody body ->
+                    div [] [ text ("Bad Body : " ++ body) ]
+    in
+    [ display ]
+
+
 
 -- API
 
+
 loadPages : Cmd Msg
-loadPages = Http.get
+loadPages =
+    Http.get
         { url = "/api/v1/pages"
         , expect = Http.expectJson GotPages (responseDecoder overviewDecoder)
         }
 
+
 createPage : Cmd Msg
-createPage = Http.post 
-    {
-        url = "/api/v1/pages",
-        body = Http.jsonBody (pageCreateEncoder "test" "/pages/article"),
-        expect = Http.expectJson CreatePage (responseDecoder De.string)
-    }
+createPage =
+    Http.post
+        { url = "/api/v1/pages"
+        , body = Http.jsonBody (pageCreateEncoder "test" "/pages/article")
+        , expect = Http.expectJson CreatePage (responseDecoder De.string)
+        }
+
 
 
 -- Json
 
+
 responseDecoder : De.Decoder data -> De.Decoder (Response data)
-responseDecoder dataDecoder = De.map2 Response
-    (De.field "data" (De.maybe dataDecoder))
-    (De.field "links" (De.list De.string))
+responseDecoder dataDecoder =
+    De.map2 Response
+        (De.field "data" (De.maybe dataDecoder))
+        (De.field "links" (De.list De.string))
+
 
 overviewDecoder : De.Decoder Overview
-overviewDecoder = De.list De.string
+overviewDecoder =
+    De.list De.string
+
 
 pageCreateEncoder : String -> String -> En.Value
-pageCreateEncoder name template = 
-    En.object [
-        ("name", En.string name),
-        ("template", En.string template)
-    ]
+pageCreateEncoder name template =
+    En.object
+        [ ( "name", En.string name )
+        , ( "template", En.string template )
+        ]
